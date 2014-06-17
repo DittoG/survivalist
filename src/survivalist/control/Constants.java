216f26f66197a17f6aba75e99e49d38d12fd3866
Jerry <Jerry@Hizzlemachine>
2014-06-16 21:54:01 -0600

    Added super constructor in the Animal class to get rid of errors in GameControl class.

--- src/survivalist/control/Constants.java
+++ src/survivalist/control/Constants.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package survivalist.control;
 
 /**
@@ -11,25 +10,25 @@ package survivalist.control;
  * @author Madison
  */
 public class Constants {
+
     public static int NUMBER_OF_INVENTORY_ITEMS = 20;
-    
+
     // Inventory Items
     // WEAPONS******************************************************************************
-    
     public final static int KNIFE = 0;
     public final static int BOW = 1;
     public final static int ARROW = 2;
     public final static int MACHETE = 3;
-    
+
     // TOOLS********************************************************************************
     public final static int AXE = 4;
     public final static int HAMMER = 5;
     public final static int FLINTANDSTONE = 6;
     public final static int GRAPPLINGHOOK = 7;
-    
+
     // INGREDIENT***************************************************************************
-    public final static int BRANCH= 8;
-    public final static int FLATROCK= 9;
+    public final static int BRANCH = 8;
+    public final static int FLATROCK = 9;
     public final static int SHARPROCK = 10;
     public final static int ANIMALSKIN = 11;
     public final static int RAWMEAT = 12;
@@ -38,23 +37,21 @@ public class Constants {
     public final static int STONE = 15;
     public final static int LOG = 16;
     public final static int GUTSTRING = 17;
-    
+
     //FOODS********************************************************************************
     public final static int BERRY = 18;
     public final static int COOKEDMEAT = 19;
-    
+
     //NPCS*********************************************************************************
-    
     public static int NUMBER_OF_NPCS;
-    
+
     public final static int UNCLEDARWIN = 20;
     public final static int AVIATORBILL = 21;
     public final static int RANGERDAN = 22;
-    
+
     //ANIMALS*******************************************************************************
-    
     public static int NUMBER_OF_ANIMALS;
-    
+
     public final static int DEER = 23;
     public final static int ELK = 24;
     public final static int RABBIT = 25;
@@ -62,22 +59,20 @@ public class Constants {
     public final static int WOLF = 27;
     public final static int MOOSE = 28;
     public final static int BEAR = 29;
-    
+
     //BUILDINGS******************************************************************************
-    
     public final static int NUMBER_OF_BUILDINGS = 30;
-    
+
     public final static int CABIN = 31;
     public final static int TOWER = 32;
     public final static int LEANTO = 33;
-    
+
     //MAP INFO*******************************************************************************
-    
     public static int MAP_ROW_COUNT;
     public static int MAP_COLUMN_COUNT;
-    
+
     public static int NUMBER_OF_SCENES;
-    
+
     public final static int FOREST = 34;
     public final static int WATER = 35;
     public final static int GRASS = 36;
@@ -86,7 +81,6 @@ public class Constants {
     public final static int ROAD = 39;
     public final static int CRASH = 40;
     public final static int STREAM = 41;
-    public final static int CRASH_SITE= 42;
-    
-    
+    public final static int CRASH_SITE = 42;
+
 }
